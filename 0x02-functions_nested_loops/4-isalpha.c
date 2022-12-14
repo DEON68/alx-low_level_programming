b16a7687b4441289808957aca435ef22bdd652bd
DEON68 <kamsyagha23@gmail.com>
2022-12-13 17:47:58 -0800

    5-sign.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * isalpha - checks for alphabets
  * @c: character to be checked
- * Return: 1 if character is a letter, 0 otherwise
+ * Return: 1 if character is a letter,0 otherwise
  */
 int _isalpha(int c)
 {
